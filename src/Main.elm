b4e8c6e8282061243c8705cd6f10dcb8bd45467e
Diego Salazar <mastermindxs@gmail.com>
2020-05-11 22:48:35 -0400

    created elm prototype search page

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,251 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, h2, img)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Browser.Navigation as Navigation
+import Browser exposing (UrlRequest)
+import Url exposing (Url)
+import Url.Parser as UrlParser exposing ((</>), Parser, s, top)
+import Bootstrap.CDN as CDN
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Button as Button
+import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Modal as Modal
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Bootstrap.Form.Select as Select
+import Bootstrap.Form.Checkbox as Checkbox
+import Bootstrap.Form.Radio as Radio
+import Bootstrap.Form.Textarea as Textarea
+import Bootstrap.Form.Fieldset as Fieldset
+import Bootstrap.Button as Button
 
 
----- MODEL ----
-
-
-type alias Model =
+type alias Flags =
     {}
 
+type alias Model =
+    { navKey : Navigation.Key
+    , page : Page
+    , navState : Navbar.State
+    , modalVisibility : Modal.Visibility
+    }
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+type Page
+    = Home
+    | Actors
+    | Movies
+    | NotFound
 
 
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = ClickedLink
+        , onUrlChange = UrlChange
+        }
 
----- UPDATE ----
+init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        ( navState, navCmd ) =
+            Navbar.initialState NavMsg
+
+        ( model, urlCmd ) =
+            urlUpdate url { navKey = key, navState = navState, page = Home, modalVisibility= Modal.hidden }
+    in
+        ( model, Cmd.batch [ urlCmd, navCmd ] )
+
+
 
 
 type Msg
-    = NoOp
+    = UrlChange Url
+    | ClickedLink UrlRequest
+    | NavMsg Navbar.State
+    | CloseModal
+    | ShowModal
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Navbar.subscriptions model.navState NavMsg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        ClickedLink req ->
+             case req of
+                 Browser.Internal url ->
+                     ( model, Navigation.pushUrl model.navKey <| Url.toString url )
+
+                 Browser.External href ->
+                     ( model, Navigation.load href )
+
+
+        UrlChange url ->
+            urlUpdate url model
+
+        NavMsg state ->
+            ( { model | navState = state }
+            , Cmd.none
+            )
+
+        CloseModal ->
+            ( { model | modalVisibility = Modal.hidden }
+            , Cmd.none
+            )
+
+        ShowModal ->
+            ( { model | modalVisibility = Modal.shown }
+            , Cmd.none
+            )
 
 
 
----- VIEW ----
+urlUpdate : Url -> Model -> ( Model, Cmd Msg )
+urlUpdate url model =
+    case decode url of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just route ->
+            ( { model | page = route }, Cmd.none )
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+decode : Url -> Maybe Page
+decode url =
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+    |> UrlParser.parse routeParser
+
+
+routeParser : Parser (Page -> a) a
+routeParser =
+    UrlParser.oneOf
+        [ UrlParser.map Home top
+        , UrlParser.map Actors (s "actors")
+        , UrlParser.map Movies (s "movies")
         ]
 
 
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Filmographio"
+    , body =
+        [ CDN.stylesheet -- creates a link tag with the Bootstrap CSS
+          , div []
+            [ menu model
+            , mainContent model
+            , modal model
+            ]
+        ]
+    }
 
----- PROGRAM ----
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+menu : Model -> Html Msg
+menu model =
+    Navbar.config NavMsg
+        |> Navbar.withAnimation
+        |> Navbar.container
+        |> Navbar.brand [ href "#" ] [ text "Filmographio" ]
+        |> Navbar.items
+            [ Navbar.itemLink [ href "#actors" ] [ text "Actors" ]
+            , Navbar.itemLink [ href "#movies" ] [ text "Movies" ]
+            ]
+        |> Navbar.view model.navState
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    Grid.container [] <|
+        case model.page of
+            Home ->
+                pageHome model
+
+            Actors ->
+                pageActors model
+
+            Movies ->
+                pageMovies model
+
+            NotFound ->
+                pageNotFound
+
+
+pageHome : Model -> List (Html Msg)
+pageHome model =
+    [ Grid.row []
+        [ Grid.col []
+            [ Form.formInline []
+                [ Input.text [ Input.attrs [ placeholder "Search" ]]
+                , Button.button
+                    [ Button.primary
+                    , Button.attrs [ class "ml-sm-2 my-2" ]
+                    ]
+                    [ text "Search" ]
+                ]
+            ]
+        ]
+    ]
+
+
+pageActors : Model -> List (Html Msg)
+pageActors model =
+    [ h2 [] [ text "Actors" ]
+    , Button.button
+        [ Button.success
+        , Button.large
+        , Button.block
+        , Button.attrs [ onClick ShowModal ]
+        ]
+        [ text "Click me" ]
+    ]
+
+
+pageMovies : Model -> List (Html Msg)
+pageMovies model =
+    [ h1 [] [ text "Movies" ]
+    , Listgroup.ul
+        [ Listgroup.li [] [ text "Alert" ]
+        , Listgroup.li [] [ text "Badge" ]
+        , Listgroup.li [] [ text "Card" ]
+        ]
+    ]
+
+
+pageNotFound : List (Html Msg)
+pageNotFound =
+    [ h1 [] [ text "Not found" ]
+    , text "Sorry couldn't find that page"
+    ]
+
+
+modal : Model -> Html Msg
+modal model =
+    Modal.config CloseModal
+        |> Modal.small
+        |> Modal.h4 [] [ text "Getting started ?" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 1" ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 2" ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.modalVisibility
