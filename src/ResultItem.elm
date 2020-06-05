abdc48f233b70df69126ca9dd2b3d6e6b74ab774
Diego Salazar <mastermindxs@gmail.com>
2020-06-04 20:28:10 -0400

    created ResultItem and started to make rendering results dynamic

---
+++ src/ResultItem.elm
@@ -0,0 +1,48 @@
+module ResultItem exposing (view)
+
+import Html exposing (Html, text, h5, div, p, small)
+import Html.Attributes exposing (href)
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Utilities.Flex as Flex
+import Bootstrap.Utilities.Spacing as Spacing
+import Bootstrap.Utilities.Size as Size
+
+type alias Model =
+    { heading : String
+    , detail : String
+    , text : String
+    , footer : String
+    }
+
+-- ListGroup.anchor
+--     [ ListGroup.attrs [ href "#", Flex.col, Flex.alignItemsStart ] ]
+--     [ div [ Flex.block, Flex.justifyBetween, Size.w100 ]
+--         [ h5 [ Spacing.mb1 ] [ text model.heading ]
+--         , small [] [ text "3 days ago" ]
+--         ]
+--     , p [ Spacing.mb1 ] [ text "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." ]
+--     , small [] [ text "Oh yea that's neat" ]
+--     ]
+-- ListGroup.anchor
+--     [ ListGroup.active
+--     , ListGroup.attrs [ href "#", Flex.col, Flex.alignItemsStart ]
+--     ]
+--     [ div [ Flex.block, Flex.justifyBetween, Size.w100 ]
+--         [ h5 [ Spacing.mb1 ] [ text "List group heading" ]
+--         , small [] [ text "3 days ago" ]
+--         ]
+--     , p [ Spacing.mb1 ] [ text "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit." ]
+--     , small [] [ text "Oh yea that's neat" ]
+--     ]
+
+view : Model -> ListGroup.CustomItem msg
+view model =
+    ListGroup.anchor
+        [ ListGroup.attrs [ href "#", Flex.col, Flex.alignItemsStart ] ]
+        [ div [ Flex.block, Flex.justifyBetween, Size.w100 ]
+            [ h5 [ Spacing.mb1 ] [ text model.heading ]
+            , small [] [ text model.detail ]
+            ]
+        , p [ Spacing.mb1 ] [ text model.text ]
+        , small [] [ text model.footer ]
+        ]
--- src/SearchPage.elm
+++ src/SearchPage.elm
@@ -1,12 +1,19 @@
 module SearchPage exposing (Model, init, pageSearch)
 
-import Html exposing (Html, text, h2, div)
-import Html.Attributes exposing (placeholder, class)
+import Html exposing (Html, text, h2, h5, div, p, small)
+import Html.Attributes exposing (placeholder, class, href)
 import Bootstrap.Grid as Grid
 import Bootstrap.Form as Form
 import Bootstrap.Button as Button
 import Bootstrap.Form.InputGroup as InputGroup
 import Bootstrap.Form.Input as Input
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Utilities.Flex as Flex
+import Bootstrap.Utilities.Spacing as Spacing
+import Bootstrap.Utilities.Size as Size
+
+import ResultItem exposing (view)
+
 
 type alias Model =
     { query : String
@@ -20,10 +27,18 @@ init =
 
 pageSearch : Html msg
 pageSearch =
+    let
+        results =
+            [ { heading = "Audrey Hepburn", detail = "3 days ago", text = "Donec id elit non mi porta", footer = "Thats the best" }
+            , { heading = "Dolly Parton", detail = "2 days ago" , text = "Maecenas sed diam eget risus varius blandit.", footer = "Oh yea that's neat" }
+            , { heading = "Natalie Portman", detail = "1 days ago" , text = "Mi porta gravida at eget maecenas sed diam eget risus varius blandit.", footer = "What else" }
+            ]
+
+    in
     div []
         [ searchForm
         , searchToolBar
-        , resultsList
+        , resultsList results
         , searchFooter
         ]
 
@@ -48,20 +63,22 @@ searchForm =
 searchToolBar =
     Grid.row []
         [ Grid.col []
-            [ h2 [] [ text "search toolbar" ]
+            [ h2 [] [ text "results toolbar" ]
             ]
         ]
 
-resultsList =
+resultsList results =
     Grid.row []
-        [ Grid.col []
-            [ h2 [] [ text "results list" ]
-            ]
+        [ Grid.col [] <| resultsListContent results
         ]
+
+resultsListContent results =
+    [ ListGroup.custom <| List.map ResultItem.view results
+    ]
 
 searchFooter =
     Grid.row []
         [ Grid.col []
-            [ h2 [] [ text "search footer" ]
+            [ h2 [] [ text "results footer" ]
             ]
         ]
